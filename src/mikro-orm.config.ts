77344634d7fc1b0bd6cc0f57561d023084cf0c3b
Kyrylo Avramenko <wkyrylo@gmail.com>
2020-08-26 20:54:21 -0700

    first commit

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { __prod__ } from './constants';
+import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
+import { Post } from './entities/Post';
+
+// console.log('dirname: ', __dirname);
+
+export default {
+    migrations: {
+        path: path.join(__dirname, './migrations'),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    entities: [Post],
+    dbName: 'lireddit',
+    user: 'postgres',
+    password: 'Barca2004Ron',
+    type: 'postgresql',
+    debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
